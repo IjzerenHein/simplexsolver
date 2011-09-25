1c0bdfc4fe69d2dbb02d940dd722aad32788e95b
djphilomath <djphilomath@cd77c9ca-163b-56f8-63d3-eb2b4a7f7817>
2011-09-25 20:43:19 +0000

    first commit

---
+++ trunk/simplex/lilb/simplex.js
@@ -0,0 +1,40 @@
+/**
+ *  1. Simplex "maximizar s.a."
+ *  2. Mostrar gráfica
+ *  *región factible
+ *  *función objetivo
+ *  *solución óptima
+ *  
+ *   Ejemplos de la documentación con 
+ *   Max. z=10x1+15x2
+ *   sa.
+ *   5x1+10x2≤6000
+ *   2.5x1+2.5x2≤2250
+ *   2x1+x2≤1200
+ */
+
+function Simplex() {}
+
+/** Bidimenxional array of the form
+ * { { 
+ */
+Simplex.prototype.variables = {}
+
+/** Gest data from web form and completes the array with the info.
+ * 
+ */
+Simplex.prototype.getData = function() {
+	
+}
+
+Simplex.prototype.solve = function() {
+	
+}
+
+Simplex.prototype.displayGraph = function() {
+	
+}
+
+S('button').click(function() {
+	// Aquí va lo que sucede cuando se da clic al botón de resolver.
+});
