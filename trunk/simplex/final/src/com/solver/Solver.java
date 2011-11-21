863c17306e0c7f17524d68c1b80e0a2428492419
djphilomath <djphilomath@cd77c9ca-163b-56f8-63d3-eb2b4a7f7817>
2011-11-21 19:38:25 +0000

    esqueleto de metodos

--- trunk/simplex/final/src/com/solver/Solver.java
+++ trunk/simplex/final/src/com/solver/Solver.java
@@ -1,5 +1,60 @@
 package com.solver;
 
 public class Solver {
+	
+	int noRestricciones = 3;
+	int noVariables = 5;
+	boolean max = true;
+	final double LT = 1;
+	final double EQ = 2;
+	final double GT = 3;
+	
+	public double temperatura;
+	public double temperaturaPaso = 0.01;
 
+	public double[] objetivo = new double[noVariables];
+	public double[][] restricciones = new double[noRestricciones][noVariables+2];
+	
+	public Solver() {
+		// TOOD: Inicializar temperatura
+	}
+	
+	public void entrada() {
+		double[] objetivo = {10, 15};
+		this.objetivo = objetivo;
+		double[] r1;
+	}
+	
+	/** Genera una solucion aleatoria dentro del rango de las restricciones (factible)*/
+	public void generaUnaSolucionInicial() {
+		
+	}
+	
+	/** disminuye siempre la temperatura en X cantidad */
+	public void actualizaTemperatura() {
+		
+	}
+	
+	/** Funcion de evaluación, calcular Z */
+	public void queTanBuenoEs() {
+		
+	}
+	
+	/** Probabilidad de aceptar */
+	public boolean boltzmann(double actual, double posible) {
+		double prob;
+		
+		if (max && posible < actual) return true;
+		if (!max && posible > actual) return true;
+		
+		prob = Math.exp(-Math.abs(posible-actual)/this.temperatura);
+		
+		return Math.random() < prob;
+	}
+	
+	public static void main(String[] args) {
+		Solver s = new Solver();
+		s.entrada();
+	}
+	
 }
